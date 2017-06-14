ca183fb72f64882dff2cd1f3cffc841d5a4c6856
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-06-14 13:22:44 -0500

    angular skeleton

---
+++ app/app.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var app = angular.module("bestivalApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider.
+   when("/", {
+      templateUrl: "partials/.html",
+      controller: "Ctrl"
+    }).
+   // when("/storytime", {
+   //    templateUrl: "partials/homepage.html",
+   //    controller: "HomepageCtrl"
+   //  }).
+
+  otherwise('/');
+});
+
+
+
+
